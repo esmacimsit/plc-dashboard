f2744a1909ec4299e87d4020bb0d8d65d7e8c63c
esmacimsit <esmanurcimsit@gmail.com>
2025-09-20 02:57:15 +0300

    composite keys added

---
+++ src/main/java/com/sistek/sos/analysis_dashboard/entities/BarcodeData.java
@@ -0,0 +1,15 @@
+package com.sistek.sos.analysis_dashboard.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "barcode_data")
+
+public class BarcodeData {
+
+}
---
+++ src/main/java/com/sistek/sos/analysis_dashboard/entities/keys/BarcodeDataId.java
@@ -0,0 +1,5 @@
+package com.sistek.sos.analysis_dashboard.entities.keys;
+
+// must if there are multiple pks
+public class BarcodeDataId {
+}
